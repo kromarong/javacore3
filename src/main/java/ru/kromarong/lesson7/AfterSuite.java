221ba6928edee93094dfe2588dab14bdd46c8ca4
kromarong <kromarong@gmail.com>
2019-03-25 14:09:59 +0300

    hw7 done

---
+++ src/main/java/ru/kromarong/lesson7/AfterSuite.java
@@ -0,0 +1,12 @@
+package ru.kromarong.lesson7;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface AfterSuite {
+    float value() default 5.0f;
+}
